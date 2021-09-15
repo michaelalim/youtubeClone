62acbcf43b49655fcb7353f8d44a7a7c00943bd2
Michael Jason Alim <lolmichael03@gmail.com>
2021-09-15 22:42:10 +0800

    transferred some html to js
    transferred on video page the like-button and everything beside it to js.

--- youtubeClone/webpages/video/index.js
+++ youtubeClone/webpages/video/index.js
@@ -2,12 +2,62 @@
 
 import { startUp } from "../../dependencies/elements/sidebar.js"
 
+// buttons on the LIKE row
+
+import { likeAreaArrs } from "./videoArrEls.js";
+
 startUp();
+
+const likeAreaContainer = document.getElementById("like-area-btns");
+
+likeAreaArrs.forEach((btns) => {
+    const likeDislikeContainer = document.createElement("div");
+    likeDislikeContainer.classList.add("like-dislike-btns");
+    likeAreaContainer.appendChild(likeDislikeContainer);
+    
+    if (btns.id === "like-btn" || btns.id === "dislike-btn"){
+        const btn = document.createElement("button");
+        btn.classList.add("btn", btns.uniqueClass);
+        btn.setAttribute("id", btns.id);
+        likeDislikeContainer.appendChild(btn);
+
+        const btnIcon = document.createElement("span");
+        // to make like button not be outlined so that it will look like the button is "LIKED"
+        if(btns.id === "like-btn"){
+            btnIcon.classList.add("material-icons");
+        } else {
+            btnIcon.classList.add("material-icons-outlined");
+        }
+        btnIcon.textContent = btns.spanName;
+        btn.appendChild(btnIcon);
+        //when the number is more than 1000, it will convert it to K, like 1000:1K
+        if (btns.number > 1000){
+            btns.number = `${btns.number / 1000}K`
+            btn.innerHTML += ` ${btns.number}`;
+        } else {
+            btn.innerHTML += ` ${btns.number}`;
+        }
+    } else {
+        const btn = document.createElement("button");
+        btn.classList.add("btn", btns.uniqueClass);
+        btn.setAttribute("id", btns.id);
+        likeAreaContainer.appendChild(btn);
+
+        const btnIcon = document.createElement("span");
+        btnIcon.classList.add("material-icons-outlined");
+        btnIcon.innerHTML = btns.spanName;
+        btn.appendChild(btnIcon);
+
+        const btnText = document.createElement("span");
+        btnText.classList.add("btnTextLikeRow");
+        btnText.textContent = btns.textname;
+        btn.appendChild(btnText);
+    }
+})
 
 const descSeeMoreBtn = document.getElementById("see-more");
 const dots = document.getElementById("dots");
 const hiddenMsgs = document.querySelectorAll(".more");
-
 
 descSeeMoreBtn.addEventListener("click", () => {
    
@@ -37,4 +87,4 @@ commentBar.addEventListener("input", () => {
         sendCommentBtn.style.background = "#333";
         sendCommentBtn.style.color = "#777";
     }
-})
+});
---
+++ youtubeClone/webpages/video/videoArrEls.js
@@ -0,0 +1,31 @@
+export const likeAreaArrs = [
+    {
+        id: "like-btn",
+        uniqueClass: "like-btn",
+        spanName: "thumb_up",
+        number: 410000
+    },
+    {
+        id: "dislike-btn",
+        uniqueClass: "dislike-btn",
+        spanName: "thumb_down",
+        number: 784
+    },
+    {
+        id: "share-btn",
+        uniqueClass: "share-btn",
+        spanName: "send",
+        textname: "Share"
+    },
+    {
+        id: "save-btn",
+        uniqueClass: "save-btn",
+        spanName: "playlist_add",
+        textname: "Save"
+    },
+    {
+        id: "others-btn",
+        uniqueClass: "others-btn",
+        spanName: "more_horiz",
+    }
+]
